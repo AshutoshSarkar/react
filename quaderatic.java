149310b10454130ffb298b49c0dee7ee30a28ce6
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2023-03-23 22:47:33 +0530

    intital commit

---
+++ quaderatic.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class quaderatic{
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Please Enter first number");
+        int a=sc.nextInt();
+        
+        System.out.println("Please Enter Second number");
+        int b= sc.nextInt();
+       
+
+        int sum = a+b;
+
+        System.out.println("The sum of two numbers is :" +sum);
+
+    
+}
+}
